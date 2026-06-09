2587f6948aa8b3b68daa27677c996d7d09e2a454
gabrielpsantos41 <gabrielsan41.dev@gmail.com>
2026-06-09 20:24:42 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+    window.addEventListener('scroll', function () {
+      const navbar = document.querySelector('.navbar');
+
+      if (window.scrollY > 50) {
+        navbar.style.padding = '12px 0';
+        navbar.style.background = 'rgba(2, 6, 23, 0.98)';
+      } else {
+        navbar.style.padding = '18px 0';
+        navbar.style.background = 'rgba(15, 23, 42, 0.95)';
+      }
+    });
